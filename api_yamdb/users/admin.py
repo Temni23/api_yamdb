5ee44ea875bb9c233a62086ff7c585bc55495936
Артём Климов <temni@mail.ru>
2023-04-11 19:55:54 +0700

    Добавлены приложения api, users и reviews, внесены настройки приложений, добавлены необходимые файлы в приложение api.

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -1,12 +1,1 @@
-# from django.contrib import admin
-#
-# from .models import User
-#
-#
-# class UsersAdmin(admin.ModelAdmin):
-#     list_display = ('pk',)
-#     # search_fields = ('text',)
-#     # list_filter = ('pub_date',)
-#     empty_value_display = '-пусто-'
-#
-# admin.site.register(User)
+from django.contrib import admin
