330b63b2933dd32673640ce44a1249a66e5a6efd
Temni23 <temni23@yandex.ru>
2023-04-12 09:18:54 +0700

    Исправлены urls головной и в приложении api.

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,3 +1,15 @@
 from django.urls import include, path
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
+
+# from .views import UserViewSet
+#
+#
+# app_name = "api"
+#
+# v1_router = DefaultRouter()
+# v1_router.register("users", UserViewSet, basename="users")
+#
+# urlpatterns = [
+#     path("v1/", include(v1_router.urls)),
+# ]
