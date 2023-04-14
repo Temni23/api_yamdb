2d1c14d1381c1ab17dce5d1bdc7329b2513a4ea9
zxcmax <maks29121@gmail.com>
2023-04-14 15:10:06 +0700

    add models for titles, categories, genres

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=255)
+    year = models.PositiveIntegerField()
+    category = models.ForeignKey(
+        Category, on_delete=models.DO_NOTHING, related_name='titles'
+    )
+    genre = models.ManyToManyField(Genre)
+
+    def __str__(self):
+        return self.name
