ae0ac3066bdf728be7ad6fd13669b8aab21f884b
Никита Цимбалов <n.tsimbalov98@gmail.com>
2023-04-14 22:10:35 +0700

    change some variables names and endpoints

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -10,17 +10,17 @@ app_name = "api"
 v1_router = DefaultRouter()
 v1_router.register("users", UserViewSet, basename="users")
 v1_router.register(
-    r'posts/(?P<post_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    r'posts/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
     CommentViewSet, basename='comments')
 
 v1_router.register(
-    r'posts/(?P<post_id>\d+)/reviews',
+    r'posts/(?P<title_id>\d+)/reviews',
     ReviewViewSet, basename='reviews')
 v1_router.register(
-    r'posts/(?P<post_id>\d+)/reviews/(?P<review_id>\d+)',
+    r'posts/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)',
     ReviewViewSet, basename='reviewedit')
 v1_router.register(
-    r'posts/(?P<post_id>\d+)/reviews/(?P<review_id>\d+)'
+    r'posts/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)'
     r'/comments/(?P<comment_id>\d+)',
     CommentViewSet, basename='commentsedit')
 urlpatterns = [
